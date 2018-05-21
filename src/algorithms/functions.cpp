01ad76db0218b549c67a771702c8120416aa5730
fizban007 <fizban007@gmail.com>
2018-05-21 11:51:57 -0400

    Put two light surfaces at 0.05 and 0.95

--- src/algorithms/functions.cpp
+++ src/algorithms/functions.cpp
@@ -3,11 +3,12 @@
 namespace Aperture {
 
 double beta_phi(double x) {
-  double b = (x - 0.1)/0.4 - 1.0;
-  if (x > 0.1 && x < 0.9) {
-    return b;
-  } else {
-    return b;
-  }
+  double b = (x - 0.05)/0.45 - 1.0;
+  return b;
+  // if (x > 0.05 && x < 0.95) {
+  //   return b;
+  // } else {
+  //   return b;
+  // }
 }
 }
