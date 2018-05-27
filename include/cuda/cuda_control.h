9f0d3e80a967c9114c13aca3aaff267050d50860
fizban007 <fizban007@gmail.com>
2018-05-27 17:10:40 -0400

    Added cuda control header preparing for next steps

---
+++ include/cuda/cuda_control.h
@@ -0,0 +1,17 @@
+#ifndef  _CUDACONTROL_H_
+#define  _CUDACONTROL_H_
+
+#ifdef __CUDACC__
+  #define HOST_DEVICE __host__ __device__
+  #define HD_INLINE __host__ __device__ 
+  // #define WITH_CUDA_ENABLED
+#else
+  #define HOST_DEVICE
+  #define HD_INLINE inline
+  // #ifdef WITH_CUDA_ENABLED
+  //   #undef WITH_CUDA_ENABLED
+  // #endif
+#endif
+
+#endif // _CUDACONTROL_H_
+
