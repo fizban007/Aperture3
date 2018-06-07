9e14c026c2e674da7f217b86361f0450da8114cf
fizban007 <fizban007@gmail.com>
2018-06-07 17:39:43 -0400

    Adapted quadmesh

--- include/data/quadmesh.h
+++ include/data/quadmesh.h
@@ -4,7 +4,7 @@
 #include <algorithm>
 #include <immintrin.h>
 #include <iomanip>
-// #include "cuda/cuda_control.h"
+#include "cuda/cuda_control.h"
 #include "data/typedefs.h"
 #include "data/vec3.h"
 
@@ -21,10 +21,10 @@ struct Quadmesh {
   Scalar lower[3];  //!< Lower limit of the grid on each direction
   Scalar sizes[3];  //!< Size of the grid in coordinate space
 
-  // int tileSize;
+  int tileSize;
   int dimension;
 
-  Quadmesh() {  //!< Default constructor
+  HOST_DEVICE Quadmesh() {  //!< Default constructor
 // Only define an empty constructor when compiling with Cuda enabled.
 // This allows declaring a quadmesh in __constant__ memory.
 #ifndef __CUDACC__
@@ -36,7 +36,7 @@ struct Quadmesh {
       lower[i] = 0.0;
       sizes[i] = 0.0;
     }
-    // tileSize = 8;
+    tileSize = 64;
     dimension = 1;
 #endif  // __CUDACC__
   }
@@ -44,10 +44,10 @@ struct Quadmesh {
   ////////////////////////////////////////////////////////////////////////////////
   ///  Constructor which only initialize dimensions.
   ////////////////////////////////////////////////////////////////////////////////
-  Quadmesh(int N1, int N2 = 1, int N3 = 1) {
-    dims[0] = std::max(1, N1);
-    dims[1] = std::max(1, N2);
-    dims[2] = std::max(1, N3);
+  HOST_DEVICE Quadmesh(int N1, int N2 = 1, int N3 = 1) {
+    dims[0] = max(1, N1);
+    dims[1] = max(1, N2);
+    dims[2] = max(1, N3);
 
     // Initialize other quantities to zero
     for (int i = 0; i < 3; i++) {
@@ -56,14 +56,14 @@ struct Quadmesh {
       lower[i] = 0.0;
       sizes[i] = delta[i] * dims[i];
     }
-    // tileSize = 8;
+    tileSize = 64;
     dimension = dim();
   }
 
   ////////////////////////////////////////////////////////////////////////////////
   ///  Assignment operator
   ////////////////////////////////////////////////////////////////////////////////
-  Quadmesh& operator=(const Quadmesh& m) {
+  HOST_DEVICE Quadmesh& operator=(const Quadmesh& m) {
     for (int i = 0; i < 3; i++) {
       dims[i] = m.dims[i];
       guard[i] = m.guard[i];
@@ -71,7 +71,7 @@ struct Quadmesh {
       lower[i] = m.lower[i];
       sizes[i] = m.sizes[i];
     }
-    // tileSize = m.tileSize;
+    tileSize = m.tileSize;
     dimension = m.dimension;
     return *this;
   }
@@ -79,7 +79,7 @@ struct Quadmesh {
   ////////////////////////////////////////////////////////////////////////////////
   ///  Comparison operator
   ////////////////////////////////////////////////////////////////////////////////
-  bool operator==(const Quadmesh& m) const {
+  HOST_DEVICE bool operator==(const Quadmesh& m) const {
     bool result = true;
     for (int i = 0; i < 3; i++) {
       result = result && (dims[i] == m.dims[i]);
@@ -87,7 +87,7 @@ struct Quadmesh {
       result = result && (sizes[i] == m.sizes[i]);
       result = result && (lower[i] == m.lower[i]);
     }
-    // result = result && (tileSize == m.tileSize);
+    result = result && (tileSize == m.tileSize);
     result = result && (dimension == m.dimension);
     return result;
   }
@@ -101,7 +101,7 @@ struct Quadmesh {
   ////////////////////////////////////////////////////////////////////////////////
   // template <int i,
   //           typename = typename std::enable_if<(i >= 0 && i < DIM)>::type>
-  int reduced_dim(int i) const { return (dims[i] - 2 * guard[i]); }
+  HD_INLINE int reduced_dim(int i) const { return (dims[i] - 2 * guard[i]); }
 
   ////////////////////////////////////////////////////////////////////////////////
   ///  Coordinate of a point inside cell n in dimension i.
@@ -114,7 +114,7 @@ struct Quadmesh {
   ///  between guard cells and physical cells. The function is only
   ///  defined for i >= 0 and i < DIM.
   ////////////////////////////////////////////////////////////////////////////////
-  Scalar pos(int i, int n, bool stagger) const {
+  HD_INLINE Scalar pos(int i, int n, bool stagger) const {
     return pos(i, n, (int)stagger);
   }
 
@@ -129,7 +129,7 @@ struct Quadmesh {
   ///  between guard cells and physical cells. The function is only
   ///  defined for i >= 0 and i < DIM.
   ////////////////////////////////////////////////////////////////////////////////
-  Scalar pos(int i, int n, int stagger) const {
+  HD_INLINE Scalar pos(int i, int n, int stagger) const {
     return pos(i, n, (Scalar)(stagger * 0.5 + 0.5));
   }
 
@@ -143,7 +143,7 @@ struct Quadmesh {
   ///  between guard cells and physical cells. The function is only
   ///  defined for i >= 0 and i < DIM.
   ////////////////////////////////////////////////////////////////////////////////
-  Scalar pos(int i, int n, Scalar pos_in_cell) const {
+  HD_INLINE Scalar pos(int i, int n, Scalar pos_in_cell) const {
     if (i < dimension)
       return (lower[i] + delta[i] * (n - guard[i] + pos_in_cell));
     else
@@ -173,7 +173,7 @@ struct Quadmesh {
   // }
 
   // Get 3D position from a linear position of a cell
-  Vec3<Scalar> pos_3d(int idx, Stagger_t stagger) const {
+  HD_INLINE Vec3<Scalar> pos_3d(int idx, Stagger_t stagger) const {
     Vec3<Scalar> result;
     result[0] = pos(0, idx % dims[0], stagger[0]);
     result[1] = pos(1, (idx / dims[0]) % dims[1], stagger[1]);
@@ -193,7 +193,7 @@ struct Quadmesh {
   ///  defined for i >= 0 and i < DIM.
   ////////////////////////////////////////////////////////////////////////////////
   template <typename T>
-  Vec3<Scalar> pos_particle(int cell_linear,
+  HD_INLINE Vec3<Scalar> pos_particle(int cell_linear,
                                       const Vec3<T>& pos_rel) const {
     Vec3<Scalar> pos_full(
         pos(0, get_c1(cell_linear), pos_rel.x),
@@ -207,7 +207,7 @@ struct Quadmesh {
   ////////////////////////////////////////////////////////////////////////////////
   ///  Upper boundary position in direction i
   ////////////////////////////////////////////////////////////////////////////////
-  Scalar upper(int i) const {
+  HD_INLINE Scalar upper(int i) const {
     return pos(i, dims[i] - guard[i] - 1, 1);
   }
 
@@ -215,7 +215,7 @@ struct Quadmesh {
   ///  Find the relative position and cell number in the dual grid
   ////////////////////////////////////////////////////////////////////////////////
   template <typename T>
-  void pos_dual(Vec3<int>& c, Vec3<T>& pos) const {
+  HD_INLINE void pos_dual(Vec3<int>& c, Vec3<T>& pos) const {
     for (int i = 0; i < dimension; i++) {
       if (pos[i] > 0.5) {
         pos[i] -= 0.5;
@@ -229,21 +229,21 @@ struct Quadmesh {
   ////////////////////////////////////////////////////////////////////////////////
   ///  Index of the point if the grid is stratified into 1 direction.
   ////////////////////////////////////////////////////////////////////////////////
-  int get_idx(int c1, int c2 = 0, int c3 = 0) const {
+  HD_INLINE HD_INLINE int get_idx(int c1, int c2 = 0, int c3 = 0) const {
     return c1 + c2 * dims[0] + c3 * dims[0] * dims[1];
   }
 
   ////////////////////////////////////////////////////////////////////////////////
   ///  Index of the point if the grid is stratified into 1 direction.
   ////////////////////////////////////////////////////////////////////////////////
-  int get_idx(const Index& idx) const {
+  HD_INLINE int get_idx(const Index& idx) const {
     return idx[0] + idx[1] * dims[0] + idx[2] * dims[0] * dims[1];
   }
 
   ////////////////////////////////////////////////////////////////////////////////
   ///  Index increment in the particular direction
   ////////////////////////////////////////////////////////////////////////////////
-  int idx_increment(int direction) const {
+  HD_INLINE HD_INLINE int idx_increment(int direction) const {
     if (direction >= dimension) return 0;
     switch (direction) {
       case 0:
@@ -260,7 +260,7 @@ struct Quadmesh {
   ////////////////////////////////////////////////////////////////////////////////
   ///  Test if a point is inside the grid.
   ////////////////////////////////////////////////////////////////////////////////
-  bool is_in_grid(int c1, int c2 = 0, int c3 = 0) const {
+  HD_INLINE bool is_in_grid(int c1, int c2 = 0, int c3 = 0) const {
     return (c1 >= 0 && c1 < dims[0]) && (c2 >= 0 && c2 < dims[1]) &&
            (c3 >= 0 && c3 < dims[2]);
   }
@@ -268,7 +268,7 @@ struct Quadmesh {
   ////////////////////////////////////////////////////////////////////////////////
   ///  Test if a point is inside the bulk of the grid, not in guard cells.
   ////////////////////////////////////////////////////////////////////////////////
-  bool is_in_bulk(int c1, int c2, int c3 = 0) const {
+  HD_INLINE bool is_in_bulk(int c1, int c2, int c3 = 0) const {
     return (c1 >= guard[0] && c1 < dims[0] - guard[0]) &&
            (c2 >= guard[1] && c2 < dims[1] - guard[1]) &&
            (c3 >= guard[2] && c3 < dims[2] - guard[2]);
@@ -277,7 +277,7 @@ struct Quadmesh {
   ////////////////////////////////////////////////////////////////////////////////
   ///  Test if a point is inside the bulk of the grid, not in guard cells.
   ////////////////////////////////////////////////////////////////////////////////
-  bool is_in_bulk(const Index& idx) const {
+  HD_INLINE bool is_in_bulk(const Index& idx) const {
     // return (idx.x >= guard[0] && idx.x < dims[0] - guard[0])
     //     && (idx.y >= guard[1] && idx.y < dims[1] - guard[1])
     //     && (idx.z >= guard[2] && idx.z < dims[2] - guard[2]);
@@ -287,12 +287,12 @@ struct Quadmesh {
   ////////////////////////////////////////////////////////////////////////////////
   ///  Test if a point is inside the bulk of the grid, not in guard cells.
   ////////////////////////////////////////////////////////////////////////////////
-  bool is_in_bulk(int c) const { return is_in_bulk(get_cell_3d(c)); }
+  HD_INLINE bool is_in_bulk(int c) const { return is_in_bulk(get_cell_3d(c)); }
 
   ////////////////////////////////////////////////////////////////////////////////
   ///  Get the size of the grid (product of all dimensions).
   ////////////////////////////////////////////////////////////////////////////////
-  int size() const {
+  HD_INLINE int size() const {
     int tmp = dims[0] * dims[1] * dims[2];
     return tmp;
   }
@@ -300,7 +300,7 @@ struct Quadmesh {
   ////////////////////////////////////////////////////////////////////////////////
   ///  Find the zone the cell belongs to (for communication purposes)
   ////////////////////////////////////////////////////////////////////////////////
-  int find_zone(int cell) const {
+  HD_INLINE int find_zone(int cell) const {
     int c1 = get_c1(cell);
     int c2 = get_c2(cell);
     int c3 = get_c3(cell);
@@ -315,26 +315,26 @@ struct Quadmesh {
   ///  Find the cell index from the global position, and get the
   ///  relative position as well.
   ////////////////////////////////////////////////////////////////////////////////
-  int find_cell(const Vec3<Scalar>& pos, Vec3<Pos_t>& rel_pos) const {
+  HD_INLINE int find_cell(const Vec3<Scalar>& pos, Vec3<Pos_t>& rel_pos) const {
     int c1 = static_cast<int>(floor((pos.x - lower[0]) / delta[0])) + guard[0];
-    if (c1 < 0 || c1 > dims[0]) {
-      std::cerr << "c1 out of range: " << c1 << std::endl;
-      c1 = 0;
-    }
+    // if (c1 < 0 || c1 > dims[0]) {
+    //   std::cerr << "c1 out of range: " << c1 << std::endl;
+    //   c1 = 0;
+    // }
     int c2 = static_cast<int>(floor((pos.y - lower[1]) / delta[1])) + guard[1];
     if (dim() < 2)
       c2 = 0;
-    else if (c2 < 0 || c2 > dims[1]) {
-      std::cerr << "c2 out of range: " << c2 << std::endl;
-      c2 = 0;
-    }
+    // else if (c2 < 0 || c2 > dims[1]) {
+    //   std::cerr << "c2 out of range: " << c2 << std::endl;
+    //   c2 = 0;
+    // }
     int c3 = static_cast<int>(floor((pos.z - lower[2]) / delta[2])) + guard[2];
     if (dim() < 3)
       c3 = 0;
-    else if (c3 < 0 || c3 > dims[2]) {
-      std::cerr << "c3 out of range: " << c3 << std::endl;
-      c3 = 0;
-    }
+    // else if (c3 < 0 || c3 > dims[2]) {
+    //   std::cerr << "c3 out of range: " << c3 << std::endl;
+    //   c3 = 0;
+    // }
     rel_pos.x = (pos.x - (c1 - guard[0]) * delta[0] - lower[0]) / delta[0];
     rel_pos.y = (pos.y - (c2 - guard[1]) * delta[1] - lower[1]) / delta[1];
     rel_pos.z = (pos.z - (c3 - guard[2]) * delta[2] - lower[2]) / delta[2];
@@ -344,26 +344,26 @@ struct Quadmesh {
   ////////////////////////////////////////////////////////////////////////////////
   ///  Get the extent of the grid. Used for interfacing with multiarrays.
   ////////////////////////////////////////////////////////////////////////////////
-  Extent extent() const {
+  HD_INLINE Extent extent() const {
     return Extent{dims[0], dims[1], dims[2]};
     //    return tmp;
   }
 
-  Extent extent_less() const {
+  HD_INLINE Extent extent_less() const {
     return Extent{dims[0] - 2 * guard[0], dims[1] - 2 * guard[1],
                   dims[2] - 2 * guard[2]};
   }
 
-  int tile_num(int tile_size) const {
+  HD_INLINE int tile_num(int tile_size) const {
     int ret = reduced_dim(0) * reduced_dim(1) * reduced_dim(2);
     for (int i = 0; i < 3; i++)
       if (dims[i] > 1) ret /= tile_size;
     return ret;
   }
 
-  int get_c1(int idx) const { return idx % dims[0]; }
-  int get_c2(int idx) const { return (idx / dims[0]) % dims[1]; }
-  int get_c3(int idx) const { return idx / (dims[0] * dims[1]); }
+  HD_INLINE int get_c1(int idx) const { return idx % dims[0]; }
+  HD_INLINE int get_c2(int idx) const { return (idx / dims[0]) % dims[1]; }
+  HD_INLINE int get_c3(int idx) const { return idx / (dims[0] * dims[1]); }
 
 #if defined(__AVX2__) && (defined(__ICC) || defined(__INTEL_COMPILER))
   __m128i get_c1(__m128i n) const {
@@ -379,11 +379,11 @@ struct Quadmesh {
   }
 #endif
 
-  Vec3<int> get_cell_3d(int idx) const {
+  HD_INLINE Vec3<int> get_cell_3d(int idx) const {
     return Vec3<int>(get_c1(idx), get_c2(idx), get_c3(idx));
   }
 
-  int tile_id(int c1, int c2, int c3, int tile_size) const {
+  HD_INLINE int tile_id(int c1, int c2, int c3, int tile_size) const {
     int tileN1 = (dims[0] > 1 ? dims[0] / tile_size : 1);
     int tileN2 = (dims[1] > 1 ? dims[1] / tile_size : 1);
     int ret = (c1 - guard[0]) / tile_size;
@@ -392,7 +392,7 @@ struct Quadmesh {
     return ret;
   }
 
-  int tile_id(int cell, int tile_size) const {
+  HD_INLINE int tile_id(int cell, int tile_size) const {
     int c1 = get_c1(cell), c2 = get_c2(cell), c3 = get_c3(cell);
     int tileN1 = (dims[0] > 1 ? dims[0] / tile_size : 1);
     int tileN2 = (dims[1] > 1 ? dims[1] / tile_size : 1);
@@ -402,7 +402,7 @@ struct Quadmesh {
     return ret;
   }
 
-  int dim() const {
+  HD_INLINE int dim() const {
     if (dims[1] <= 1 && dims[2] <= 1)
       return 1;
     else if (dims[2] <= 1)
