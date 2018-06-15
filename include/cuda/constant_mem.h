50f555e80e50741da179d27ffb1ed46735366cad
fizban007 <fizban007@gmail.com>
2018-06-15 11:33:10 -0400

    Correctly handle charge and mass of each species

--- include/cuda/constant_mem.h
+++ include/cuda/constant_mem.h
@@ -7,12 +7,16 @@
 
 namespace Aperture {
 
-// #ifdef __NVCC__
-
+// This is the simulation parameters in constant memory
 extern __device__ __constant__ SimParamsBase dev_params;
+// This is the mesh structure in constant memory
 extern __device__ __constant__ Quadmesh dev_mesh;
-
-// #endif // __NVCC__
+// This is the charge for each species, up to 8 because we use 3 bits to
+// represent species
+extern __device__ __constant__ float dev_charges[8];
+// This is the mass for each species, up to 8 because we use 3 bits to
+// represent species
+extern __device__ __constant__ float dev_masses[8];
 
 }
 
