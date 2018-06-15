ebd6d8f8a77c3fddd00d2a41a8c450f7ced6ab85
fizban007 <fizban007@gmail.com>
2018-06-15 11:32:14 -0400

    Added a pointer cache to sim_data

--- src/sim_data.cu
+++ src/sim_data.cu
@@ -1,4 +1,5 @@
 #include "sim_data.h"
+#include "cuda/cudaUtility.h"
 
 namespace Aperture {
 
@@ -12,15 +13,20 @@ SimData::SimData(const Environment& e) :
     env(e), E(env.local_grid()),
     B(env.local_grid()),
     J(env.local_grid()),
-    particles(env),
-    photons(env) {
+    particles(env.params()) {
+  // photons(env) {
   // initialize(env);
-  num_species = 3;
+  num_species = env.params().num_species;
   E.initialize();
   B.initialize();
   J.initialize();
+
+  // Initialize the pointer array that contains the device pointers
+  CudaSafeCall(cudaMallocManaged(&rho_ptrs, num_species*sizeof(Scalar*)));
+
   for (int i = 0; i < num_species; i++) {
     Rho.emplace_back(env.local_grid());
+    rho_ptrs[i] = Rho[i].ptr();
     Rho_avg.emplace_back(env.local_grid());
     J_s.emplace_back(env.local_grid());
     J_avg.emplace_back(env.local_grid());
@@ -29,9 +35,12 @@ SimData::SimData(const Environment& e) :
 
 }
 
-SimData::~SimData() {}
+SimData::~SimData() {
+  cudaFree(rho_ptrs);
+}
 
 void
 SimData::initialize(const Environment& env) {}
+
 
 }
