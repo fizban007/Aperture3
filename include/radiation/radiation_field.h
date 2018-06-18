e48bd42efb22cacd45d240cfe219fd43b3dc852d
fizban007 <fizban007@gmail.com>
2018-06-18 18:56:26 -0400

    Added inverse compton module

---
+++ include/radiation/radiation_field.h
@@ -0,0 +1,16 @@
+#ifndef _RADIATION_FIELD_H_
+#define _RADIATION_FIELD_H_
+
+namespace Aperture {
+
+class RadiationField
+{
+ public:
+  RadiationField();
+  virtual ~RadiationField();
+}; // ----- end of class RadiationField -----
+
+
+}
+
+#endif  // _RADIATION_FIELD_H_
