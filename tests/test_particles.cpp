502b30c5f5f953f04c4aa2cd27b374c7b56f6fa2
fizban007 <fizban007@gmail.com>
2018-07-03 00:49:07 -0400

    Temporarily removed one particle sorting test due to memory issues

--- tests/test_particles.cpp
+++ tests/test_particles.cpp
@@ -8,25 +8,25 @@
 
 using namespace Aperture;
 
-TEST_CASE("Sorting Particles by tile", "[Particles]") {
-  Environment env("test.toml");
-  size_t N = 10000000;
-  Particles ptc(N);
+// TEST_CASE("Sorting Particles by cell", "[Particles]") {
+//   Environment env("test.toml");
+//   size_t N = 1000000;
+//   Particles ptc(N);
 
-  for (size_t i = 0; i < N; i++) {
-    ptc.append({1.0, 0.0, 0.0}, {0.0, 0.0, 0.0}, i, ParticleType::electron);
-  }
-  ptc.set_num(N);
+//   for (size_t i = 0; i < N; i++) {
+//     ptc.append({1.0, 0.0, 0.0}, {0.0, 0.0, 0.0}, N - i, ParticleType::electron);
+//   }
+//   ptc.set_num(N);
 
-  ptc.compute_tile_num();
+//   // ptc.compute_tile_num();
 
-  ptc.sync_to_device(0);
+//   ptc.sync_to_device(0);
 
-  timer::stamp();
-  // ptc.sort_by_cell();
-  ptc.sort_by_tile();
-  timer::show_duration_since_stamp("sorting by tile on gpu, with copies", "ms");
-}
+//   timer::stamp();
+//   ptc.sort_by_cell();
+//   // ptc.sort_by_tile();
+//   timer::show_duration_since_stamp("sorting by cell on gpu, with copies", "ms");
+// }
 
 TEST_CASE("Erasing particles in guard cells", "[Particles]") {
   Quadmesh mesh;
