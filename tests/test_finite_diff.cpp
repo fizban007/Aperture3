6a4a30930ada58528a036e31ab6ebd4e0e6dda0c
fizban007 <fizban007@gmail.com>
2018-07-03 01:05:49 -0400

    Added test for div

--- tests/test_finite_diff.cpp
+++ tests/test_finite_diff.cpp
@@ -9,31 +9,32 @@
 
 using namespace Aperture;
 
-TEST_CASE("Curl", "[FiniteDiff]") {
-  Environment env("test_diff.toml");
+class FiniteDiffTests {
+ protected:
+  Environment env;
+  VectorField<Scalar> u, v;
+  ScalarField<Scalar> f;
+  const Quadmesh& mesh;
 
-  VectorField<Scalar> u(env.local_grid());
-  VectorField<Scalar> v(env.local_grid());
-  auto& mesh = env.local_grid().mesh();
+ public:
+  FiniteDiffTests() :
+      env("test_diff.toml"),
+      u(env.local_grid()),
+      v(env.local_grid()),
+      f(env.local_grid()),
+      mesh(env.local_grid().mesh()) {
+    // Set everything to zero
+    u.initialize();
+    v.initialize();
+    f.initialize();
+  }
+};
+
+TEST_CASE_METHOD(FiniteDiffTests, "Curl", "[FiniteDiff]") {
   int half = mesh.dims[0] / 2;
-
-  // Set everything to zero
-  u.initialize();
-  v.initialize();
 
   CHECK(u(0, half, half, half) == 0.0f);
   CHECK(v(0, half, half, half) == 0.0f);
-
-  // CHECK(mesh.dims[0] == 258);
-  // CHECK(mesh.dims[1] == 258);
-  // CHECK(mesh.dims[2] == 258);
-  // CHECK(mesh.sizes[0] == Approx(1.0f));
-  // CHECK(mesh.sizes[1] == Approx(1.0f));
-  // CHECK(mesh.sizes[2] == Approx(1.0f));
-  // CHECK(mesh.delta[0] == Approx(1.0f/256.f));
-  // CHECK(mesh.delta[1] == Approx(1.0f/256.f));
-  // CHECK(mesh.delta[2] == Approx(1.0f/256.f));
-
 
   // Initialize field components
   u.initialize(0, [](Scalar x1, Scalar x2, Scalar x3) {
@@ -57,6 +58,31 @@ TEST_CASE("Curl", "[FiniteDiff]") {
     for (int j = mesh.guard[1]; j < mesh.dims[1] - mesh.guard[1]; j++) {
       for (int i = mesh.guard[0]; i < mesh.dims[0] - mesh.guard[0]; i++) {
         REQUIRE(v(2, i, j, k) == Approx(-1.0f));
+      }
+    }
+  }
+}
+
+TEST_CASE_METHOD(FiniteDiffTests, "Div", "[FiniteDiff]") {
+  // Initialize field components
+  u.initialize(0, [](Scalar x1, Scalar x2, Scalar x3) {
+                    return 3.0 * x1;
+                  });
+  u.initialize(1, [](Scalar x1, Scalar x2, Scalar x3) {
+                    return 2.0 * x2;
+                  });
+
+  timer::stamp();
+  // Compute the curl and add the result to v
+  div(f, u);
+  // Wait for GPU to finish before accessing on host
+  cudaDeviceSynchronize();
+  timer::show_duration_since_stamp("Taking div", "ms");
+
+  for (int k = mesh.guard[2]; k < mesh.dims[2] - mesh.guard[2]; k++) {
+    for (int j = mesh.guard[1]; j < mesh.dims[1] - mesh.guard[1]; j++) {
+      for (int i = mesh.guard[0]; i < mesh.dims[0] - mesh.guard[0]; i++) {
+        REQUIRE(f(i, j, k) == Approx(5.0f));
       }
     }
   }
