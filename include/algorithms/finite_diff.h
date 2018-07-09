615c257946c150718263ad7a53f121c1f31e2e4d
fizban007 <fizban007@gmail.com>
2018-07-09 01:35:59 -0400

    Streamlined finite_diff and added two functions for FFE

--- include/algorithms/finite_diff.h
+++ include/algorithms/finite_diff.h
@@ -5,13 +5,16 @@
 
 namespace Aperture {
 
-void curl(VectorField<Scalar>& result, const VectorField<Scalar>& u);
-void curl_2(VectorField<Scalar>& result, const VectorField<Scalar>& u);
-void div(ScalarField<Scalar>& result, const VectorField<Scalar>& u);
-void div_2(ScalarField<Scalar>& result, const VectorField<Scalar>& u);
-void grad(VectorField<Scalar>& result, const ScalarField<Scalar>& f);
-void grad_2(VectorField<Scalar>& result, const ScalarField<Scalar>& f);
-
+void curl_add(VectorField<Scalar>& result, const VectorField<Scalar>& u, Scalar q = 1.0);
+void curl(VectorField<Scalar>& result, const VectorField<Scalar>& u, Scalar q = 1.0);
+void div_add(ScalarField<Scalar>& result, const VectorField<Scalar>& u, Scalar q = 1.0);
+void div(ScalarField<Scalar>& result, const VectorField<Scalar>& u, Scalar q = 1.0);
+void grad_add(VectorField<Scalar>& result, const ScalarField<Scalar>& f, Scalar q = 1.0);
+void grad(VectorField<Scalar>& result, const ScalarField<Scalar>& f, Scalar q = 1.0);
+void ffe_edotb(ScalarField<Scalar>& result, const VectorField<Scalar>& E,
+               const VectorField<Scalar>& B, Scalar q = 1.0);
+void ffe_j(VectorField<Scalar>& result, const ScalarField<Scalar>& tmp_f,
+           const VectorField<Scalar>& E, const VectorField<Scalar>& B, Scalar q = 1.0);
 }
 
 #endif  // _FINITE_DIFF_H_
