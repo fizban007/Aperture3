3b6aea51d25e9b15415251505d62f72d67031277
fizban007 <fizban007@gmail.com>
2018-09-05 16:33:04 -0400

    Added a simple 1D array class

---
+++ src/data/array.h
@@ -0,0 +1,93 @@
+#ifndef _ARRAY_H_
+#define _ARRAY_H_
+
+#include <type_traits>
+#include <algorithm>
+#include <cassert>
+
+#include "cuda_runtime.h"
+
+namespace Aperture {
+
+/// This is a simple 1D array wrapping a cuda device pointer
+template <typename T>
+class Array {
+ public:
+  typedef T data_type;
+  typedef T* ptr_type;
+  typedef Array<T> self_type;
+
+  Array();
+
+  explicit Array(size_t length, int devId = 0);
+  Array(const self_type& other);
+  Array(self_type&& other);
+
+  ~Array();
+
+  /// Assignment operators for copying
+  self_type& operator=(const self_type& other);
+
+  /// Move assignment operator
+  self_type& operator=(self_type&& other);
+
+  /// Linearized indexing operator, read only
+  const data_type& operator[](int idx) const {
+    assert(idx >= 0 && idx < m_length);
+    return m_data_h[idx];
+  }
+
+  /// Linearized indexing operator, read and write
+  data_type& operator[](int idx) {
+    assert(idx >= 0 && idx < m_length);
+    return m_data_h[idx];
+  }
+
+  /// Allocate memory on both host and device
+  void alloc_mem(size_t N, int deviceId = 0);
+
+  /// Free memory on both host and device
+  void free_mem();
+
+  /// Sync the content between host and device
+  void sync_to_device(int devId);
+
+  /// Sync the content between host and device
+  void sync_to_device();
+
+  /// Sync the content between host and device
+  void sync_to_host();
+
+  /// Set the whole array to a single initial value on the host
+  void assign(const data_type& value);
+
+  /// Set the whole array to a single initial value through device kernel
+  void assign_dev(const data_type& value);
+
+  /// Set part of the array to a single initial value on the host
+  void assign(const data_type& value, size_t num);
+
+  /// Set part of the array to a single initial value through device kernel
+  void assign_dev(const data_type& value, size_t num);
+
+  /// Resize the array.
+  void resize(int length, int deviceId = 0);
+
+  size_t length() const { return m_length; }
+  ptr_type data() { return m_data_h; }
+  const T* data() const { return m_data_h; }
+  ptr_type data_d() { return m_data_d; }
+  const T* data_d() const { return m_data_d; }
+  int devId() const { return m_devId; }
+
+ private:
+  ptr_type m_data_d = nullptr;
+  ptr_type m_data_h = nullptr;
+
+  size_t m_length = 0;
+  int m_devId = 0;
+};
+
+}
+
+#endif  // _ARRAY_H_
---
+++ src/data/detail/array_impl.hpp
@@ -0,0 +1,137 @@
+#ifndef _ARRAY_IMPL_H_
+#define _ARRAY_IMPL_H_
+
+#include "data/array.h"
+#include "utils/memory.h"
+#include "utils/logger.h"
+#include "cuda/cudaUtility.h"
+#include "cuda_runtime.h"
+#include <algorithm>
+#include <thrust/fill.h>
+#include <thrust/device_ptr.h>
+
+namespace Aperture {
+
+template <typename T>
+Array<T>::Array() {}
+
+template <typename T>
+Array<T>::Array(size_t length, int devId) {
+  alloc_mem(length, devId);
+}
+
+template <typename T>
+Array<T>::Array(const self_type& other) {
+  alloc_mem(other.m_length, other.m_devId);
+}
+
+template <typename T>
+Array<T>::Array(self_type&& other) {
+  m_data_d = other.m_data_d;
+  m_data_h = other.m_data_h;
+  m_length = other.m_length;
+  m_devId = other.m_devId;
+  // Need to set nullptr or the pointers will be freed
+  other.m_data_h = nullptr;
+  other.m_data_d = nullptr;
+}
+
+template <typename T>
+Array<T>::~Array() {
+  free_mem();
+}
+
+template <typename T>
+void
+Array<T>::alloc_mem(size_t N, int deviceId) {
+  if (m_data_d != nullptr || m_data_h != nullptr)
+    free_mem();
+  m_devId = deviceId;
+  CudaSafeCall(cudaSetDevice(m_devId));
+  CudaSafeCall(cudaMalloc(&m_data_d, N*sizeof(T)));
+  m_data_h = new T[N];
+  m_length = N;
+}
+
+template <typename T>
+void
+Array<T>::free_mem() {
+  CudaSafeCall(cudaSetDevice(m_devId));
+  if (m_data_d != nullptr) {
+    CudaSafeCall(cudaFree(m_data_d));
+    m_data_d = nullptr;
+  }
+  if (m_data_h != nullptr) {
+    delete[] m_data_h;
+    m_data_h = nullptr;
+  }
+}
+
+/// Sync the content between host and device
+template <typename T>
+void
+Array<T>::sync_to_device(int devId) {
+  CudaSafeCall(cudaSetDevice(devId));
+  CudaSafeCall(cudaMemcpy(m_data_d, m_data_h, m_length * sizeof(T),
+                          cudaMemcpyHostToDevice));
+}
+
+/// Sync the content between host and device
+template <typename T>
+void
+Array<T>::sync_to_device() {
+  sync_to_device(m_devId);
+}
+
+/// Sync the content between host and device
+template <typename T>
+void
+Array<T>::sync_to_host() {
+  CudaSafeCall(cudaSetDevice(m_devId));
+  CudaSafeCall(cudaMemcpy(m_data_h, m_data_d, m_length * sizeof(T),
+                          cudaMemcpyDeviceToHost));
+}
+
+/// Set part of the array to a single initial value on the host
+template <typename T>
+void
+Array<T>::assign(const data_type& value, size_t num) {
+  if (num > m_length) num = m_length;
+  std::fill_n(m_data_h, num, value);
+}
+
+/// Set part of the array to a single initial value through device kernel
+template <typename T>
+void
+Array<T>::assign_dev(const data_type& value, size_t num) {
+  if (num > m_length) num = m_length;
+  thrust::device_ptr<T> ptr = thrust::device_pointer_cast(m_data_h);
+  thrust::fill_n(ptr, num, value);
+}
+
+/// Set the whole array to a single initial value on the host
+template <typename T>
+void
+Array<T>::assign(const data_type& value) {
+  assign(value, m_length);
+}
+
+/// Set the whole array to a single initial value through device kernel
+template <typename T>
+void
+Array<T>::assign_dev(const data_type& value) {
+  assign_dev(value, m_length);
+}
+
+/// Resize the array.
+template <typename T>
+void
+Array<T>::resize(int length, int deviceId) {
+  free_mem();
+  alloc_mem(length, deviceId);
+}
+
+}
+
+
+#endif  // _ARRAY_IMPL_H_
