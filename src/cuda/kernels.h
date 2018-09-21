4d1559a939a40c696ae0c53f8f893bfad3527949
fizban007 <fizban007@gmail.com>
2018-09-21 14:39:25 -0400

    Added function to compute energy histogram

--- src/cuda/kernels.h
+++ src/cuda/kernels.h
@@ -8,6 +8,8 @@ void compute_tile(uint32_t* tile, const uint32_t* cell, size_t num);
 
 void erase_ptc_in_guard_cells(uint32_t* cell, size_t num);
 
+void compute_energy_histogram(uint32_t* hist, const Scalar* E, size_t num, int num_bins, Scalar Emax);
+
 }  // namespace Aperture
 
 #endif  // _KERNELS_H_
--- src/data/photons.cu
+++ src/data/photons.cu
@@ -8,6 +8,24 @@
 
 namespace Aperture {
 
+namespace Kernels {
+
+__global__ void
+compute_photon_energies(const Scalar* p1, const Scalar* p2,
+                        const Scalar* p3, Scalar* E, size_t num) {
+  for (int i = blockIdx.x * blockDim.x + threadIdx.x; i < num;
+       i += blockDim.x * gridDim.x) {
+    if (i < num) {
+      Scalar p1p = p1[i];
+      Scalar p2p = p2[i];
+      Scalar p3p = p3[i];
+      E[i] = std::sqrt(p1p * p1p + p2p * p2p + p3p * p3p);
+    }
+  }
+}
+
+}
+
 Photons::Photons() {}
 
 Photons::Photons(std::size_t max_num)
@@ -20,5 +38,14 @@ Photons::Photons(const SimParams& params)
     : ParticleBase<single_photon_t>(params.max_photon_number) {}
 
 Photons::~Photons() {}
+
+void
+Photons::compute_energies() {
+  Kernels::compute_photon_energies<<<512, 512>>>
+      (m_data.p1, m_data.p2, m_data.p3, m_data.E, m_number);
+  // Wait for GPU to finish
+  cudaDeviceSynchronize();
+  CudaCheckError();
+}
 
 }  // namespace Aperture
