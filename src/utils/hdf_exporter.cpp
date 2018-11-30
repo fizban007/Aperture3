7108566e994d0afa7939eac1c78d703cee02d74f
fizban007 <fizban007@gmail.com>
2018-11-30 10:54:06 -0500

    Added copy src dir function to the hdf exporter

--- src/utils/hdf_exporter.cpp
+++ src/utils/hdf_exporter.cpp
@@ -23,6 +23,52 @@ using namespace HighFive;
 
 namespace Aperture {
 
+bool
+copyDir(boost::filesystem::path const& source,
+        boost::filesystem::path const& destination) {
+  namespace fs = boost::filesystem;
+  try {
+    // Check whether the function call is valid
+    if (!fs::exists(source) || !fs::is_directory(source)) {
+      std::cerr << "Source directory " << source.string()
+                << " does not exist or is not a directory." << '\n';
+      return false;
+    }
+    if (fs::exists(destination)) {
+      std::cerr << "Destination directory " << destination.string()
+                << " already exists." << '\n';
+      return false;
+    }
+    // Create the destination directory
+    if (!fs::create_directory(destination)) {
+      std::cerr << "Unable to create destination directory"
+                << destination.string() << '\n';
+      return false;
+    }
+  } catch (fs::filesystem_error const& e) {
+    std::cerr << e.what() << '\n';
+    return false;
+  }
+  // Iterate through the source directory
+  for (fs::directory_iterator file(source);
+       file != fs::directory_iterator(); ++file) {
+    try {
+      fs::path current(file->path());
+      if (fs::is_directory(current)) {
+        // Found directory: Recursion
+        if (!copyDir(current, destination / current.filename())) {
+          return false;
+        }
+      } else {
+        // Found file: Copy
+        fs::copy_file(current, destination / current.filename());
+      }
+    } catch (fs::filesystem_error const& e) {
+      std::cerr << e.what() << '\n';
+    }
+  }
+  return true;
+}
 // DataExporter::DataExporter() {}
 
 DataExporter::DataExporter(const SimParams &params,
@@ -38,6 +84,7 @@ DataExporter::DataExporter(const SimParams &params,
   boost::filesystem::create_directories(rootPath, returnedError);
   if (outputDirectory.back() != '/') outputDirectory.push_back('/');
   copyConfigFile();
+  copySrc();
 
   // Setup the grid
   if (params.coord_system == "Cartesian") {
@@ -90,6 +137,14 @@ DataExporter::copyConfigFile() {
   boost::filesystem::copy_file(
       m_params.conf_file, path,
       boost::filesystem::copy_option::overwrite_if_exists);
+}
+
+void
+DataExporter::copySrc() {
+  boost::filesystem::path src_path("./src");
+  boost::filesystem::path dest_path(outputDirectory + "src");
+
+  copyDir(src_path, dest_path);
 }
 
 bool
