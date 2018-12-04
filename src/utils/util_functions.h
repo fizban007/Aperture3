455caf6ee64667e8fe61989b9fef53ae05f8e766
fizban007 <fizban007@gmail.com>
2018-12-04 12:12:36 -0500

    Better axis boundary fields and ptrAddr function

--- src/utils/util_functions.h
+++ src/utils/util_functions.h
@@ -13,6 +13,11 @@ ptrAddr(cudaPitchedPtr p, size_t offset) {
   return (Scalar*)((char*)p.ptr + offset);
 }
 
+HD_INLINE Scalar*
+ptrAddr(cudaPitchedPtr p, int i, int j) {
+  return (Scalar*)((char*)p.ptr + j * p.pitch + i * sizeof(Scalar));
+}
+
 HD_INLINE double*
 ptrAddr_d(cudaPitchedPtr p, size_t offset) {
   return (double*)((char*)p.ptr + offset);
