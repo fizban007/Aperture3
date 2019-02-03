19db79a9697eb85f6f1696c44d91c73cbc1e21c1
fizban007 <fizban007@gmail.com>
2019-02-03 10:40:37 -0500

    Renamed Array to cu_array

--- src/cuda/data/array.cu
+++ src/cuda/data/array.cu
@@ -2,16 +2,16 @@
 
 namespace Aperture {
 
-template class Array<long long>;
-template class Array<long>;
-template class Array<int>;
-template class Array<short>;
-template class Array<char>;
-template class Array<uint16_t>;
-template class Array<uint32_t>;
-template class Array<uint64_t>;
-template class Array<float>;
-template class Array<double>;
-template class Array<long double>;
+template class cu_array<long long>;
+template class cu_array<long>;
+template class cu_array<int>;
+template class cu_array<short>;
+template class cu_array<char>;
+template class cu_array<uint16_t>;
+template class cu_array<uint32_t>;
+template class cu_array<uint64_t>;
+template class cu_array<float>;
+template class cu_array<double>;
+template class cu_array<long double>;
 
 }  // namespace Aperture
