9f2a790e8ee5bbbb7f74414eefeb8c7b8c29a465
fizban007 <fizban007@gmail.com>
2019-02-11 16:54:21 -0500

    Added device function header for IC

---
+++ src/cuda/radiation/rt_ic_dev.h
@@ -0,0 +1,19 @@
+#include "core/typedefs.h"
+
+namespace Aperture {
+
+__device__ int find_n_gamma(Scalar gamma);
+
+__device__ int find_n_e1(Scalar e1);
+
+__device__ int binary_search(float u, Scalar* array, int size,
+                             Scalar& l, Scalar& h);
+
+__device__ int binary_search(float u, int n, cudaPitchedPtr array,
+                             Scalar& l, Scalar& h);
+
+__device__ Scalar gen_photon_e(Scalar gamma, curandState* state);
+
+__device__ Scalar find_ic_rate(Scalar gamma);
+
+}  // namespace Aperture
