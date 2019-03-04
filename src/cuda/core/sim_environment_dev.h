e2b862497efc057a3f0c7d7ac12925267991ed3a
fizban007 <fizban007@gmail.com>
2019-03-04 15:37:56 -0500

    Changed init_bg_fields to be in sim_data

--- src/cuda/core/sim_environment_dev.h
+++ src/cuda/core/sim_environment_dev.h
@@ -3,17 +3,18 @@
 
 #include "commandline_args.h"
 #include "config_file.h"
+#include "core/grid.h"
+#include "cuda/data/fields_dev.h"
+#include "sim_environment.h"
 #include "sim_params.h"
+#include "utils/logger.h"
 #include <memory>
 #include <random>
 #include <string>
+#include <vector>
 // #include "core/domain_info.h"
-#include "core/grid.h"
-#include "cuda/data/fields_dev.h"
 // #include "utils/hdf_exporter.h"
 // #include "utils/mpi_comm.h"
-#include "utils/logger.h"
-#include "sim_environment.h"
 
 namespace Aperture {
 
@@ -34,47 +35,8 @@ class cu_sim_environment : public sim_environment {
   cu_sim_environment(cu_sim_environment const&) = delete;
   cu_sim_environment& operator=(cu_sim_environment const&) = delete;
 
-  // void set_initial_condition(cu_sim_data& data);
-  // void set_initial_condition(cu_sim_data& data, const Index& start, const
-  // Extent& extent); void setup_domain(int num_nodes); void
-  // setup_domain(int dimx, int dimy, int dimz = 1); void
-  // setup_local_grid(Grid& local_grid, const Grid& super_grid,
-  //                       const DomainInfo& info);
-
-  // void set_initial_condition(InitialCondition* ic);
-
-  // void add_output(const std::string& name);
   void init_bg_fields(cu_sim_data& data);
 
-  // float gen_rand() { return m_dist(m_generator); }
-
-  // data access methods
-  // const CommandArgs& args() const { return m_args; }
-  // const SimParams& conf() const { return m_conf_file.data(); }
-  // SimParams& params() { return m_params; }
-  // const SimParams& params() const { return m_params; }
-  // const ConfigFile& conf_file() const { return m_conf_file; }
-  // const Grid& grid() const { return *m_grid; }
-  // const Grid& local_grid() const { return *m_grid; }
-  // const Quadmesh& mesh() const { return m_grid->mesh(); }
-  // cu_vector_field<Scalar>& E_bg() { return m_Ebg; }
-  // cu_vector_field<Scalar>& B_bg() { return m_Bbg; }
-
-  // const Grid& local_grid() const { return m_local_grid; }
-  // const Grid& local_grid_dual() const { return m_local_grid_dual; }
-  // const Grid& super_grid() const { return m_super_grid; }
-  // const Grid& data_grid() const { return m_data_grid; }
-  // MetricType metric_type() const { return m_metric_type; }
-
-  // DataExporter& exporter() { return *m_exporter; }
-  // const MPICommWorld& world() const { return m_comm->world(); }
-  // const MPICommCartesian& cartesian() const { return
-  // m_comm->cartesian(); } const DomainInfo& domain_info() const {
-  // return m_domain_info; }
-  // // const BoundaryConditions& boundary_conditions() const { return
-  // m_bc; }
-  // // const InitialCondition& initial_condition() const { return
-  // *m_ic; }
   void check_dev_mesh(Quadmesh& mesh);
   void check_dev_params(SimParams& params);
 
@@ -83,11 +45,9 @@ class cu_sim_environment : public sim_environment {
   void load_from_snapshot(const std::string& snapshot_file);
 
  private:
-  // cu_sim_environment() {}
   void setup_env();
 
-  // cu_vector_field<Scalar> m_Ebg;
-  // cu_vector_field<Scalar> m_Bbg;
+  std::vector<int> m_dev_ids;
 };  // ----- end of class cu_sim_environment -----
 }  // namespace Aperture
 
