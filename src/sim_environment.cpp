cbeed582ecddf83b97fe3aaa2eafcead9495d214
fizban007 <fizban007@gmail.com>
2019-03-25 10:17:28 -0400

    more progress on domain decomposition

--- src/sim_environment.cpp
+++ src/sim_environment.cpp
@@ -1,17 +1,17 @@
 #include "sim_environment.h"
-#include "sim_data.h"
+#include "fmt/format.h"
 #include "grids/grid_log_sph.h"
+#include "sim_data.h"
 #include "utils/logger.h"
 #include <boost/filesystem.hpp>
 #include <memory>
-#include "fmt/format.h"
 // #include "domain_communicator.h"
 
 namespace Aperture {
 
 sim_environment::sim_environment(int* argc, char*** argv)
     : m_generator(), m_dist(0.0, 1.0) {
-  // m_comm = std::make_unique<MPIComm>(argc, argv);
+  m_comm = std::make_unique<MPIComm>(argc, argv);
 
   // Read in command line configuration
   // Handle the case of wrong command line arguments, exit gracefully
@@ -67,15 +67,10 @@ sim_environment::~sim_environment() {}
 
 void
 sim_environment::setup_env() {
-  // Init a dummy grid to store the mesh of the grid on this node
-  m_grid.reset(new Grid());
-  m_grid->init(m_params);
-
-  // std::cout << m_grid.mesh().dims[0] << ", " << m_grid.mesh().dims[1]
-  // << std::endl; std::cout << "size of quadmesh is " <<
-  // sizeof(Quadmesh) << std::endl; cudaMemcpyToSymbol(dev_mesh,
-  // (void*)m_grid.mesh_ptr(), sizeof(Quadmesh)); CudaCheckError();
-  // init_dev_mesh(*(m_grid->mesh_ptr()));
+  // Init a dummy grid to store the geometry of the overall grid for the
+  // simulation
+  m_super_grid.reset(new Grid());
+  m_super_grid->init(m_params);
 
   // Setup particle charges and masses
   for (int i = 0; i < 8; i++) {
@@ -88,18 +83,11 @@ sim_environment::setup_env() {
     m_q_over_m[i] = m_charges[i] / m_masses[i];
   }
 
-  // Obtain the metric type and setup the grid mesh
-  // m_metric_type = parse_metric(m_conf_file.data().metric);
-  // NOTE: parsing the grid does not create the cache arrays.
-  // m_super_grid.parse(m_conf_file.data().grid_config);
-  // m_data_super_grid.parse(m_conf_file.data().data_grid_config);
+  // Setup the domain
+  setup_domain(m_comm->world().size());
 
-  // setup the domain
-  // setup_domain(m_args.dimx(), m_args.dimy(), m_args.dimz());
-
-  // setup the local grid and the local data output grid
-  // setup_local_grid(m_local_grid, m_super_grid, m_domain_info);
-  // setup_local_grid(m_data_grid, m_data_super_grid, m_domain_info);
+  // Setup the local grid and the local data output grid
+  setup_local_grid();
 
   // Now we need to decide what to initiate depending on what is the
   // user configuration. Need to discuss this and think it through.
@@ -130,138 +118,129 @@ sim_environment::setup_env() {
 }
 
 void
-sim_environment::setup_local_grid(Grid& local_grid,
-                                  const Grid& super_grid,
-                                  const domain_info& info) {
-  // m_grid->init(m_params);
+sim_environment::setup_domain(int num_nodes) {
+  int ndims = m_grid->dim();
+
+  // Start to create dims from scratch
+  int dims[3] = {1, 1, 1};
+  for (int i = 0; i < ndims; ++i) dims[i] = 0;
+
+  m_comm->cartesian().create_dims(num_nodes, ndims, dims);
+
+  if (ndims == 1) setup_domain(dims[0]);
+  if (ndims == 2) setup_domain(dims[0], dims[1]);
+  if (ndims == 3) setup_domain(dims[0], dims[1], dims[2]);
 }
 
-// void
-// sim_environment::setup_domain(int num_nodes) {
-//   int ndims = m_super_grid.dim();
+void
+sim_environment::setup_domain(int dimx, int dimy, int dimz) {
+  // First create Cartesian rank group
+  bool periodic[3];
+  for (int i = 0; i < 3; i++) {
+    periodic[i] = m_params.periodic_boundary[i];
+  }
+  int dims[3]{dimx, dimy, dimz};
+  auto cartesian_ranks =
+      m_comm->get_cartesian_members(dimx * dimy * dimz);
+  m_comm->cartesian().create_cart(m_super_grid->dim(), dims, periodic,
+                                  cartesian_ranks);
+  //   // if (!m_comm->cartesian().is_null())
+  //   //   m_domain_info.state = ProcessState::primary;
 
-//   //   if (total_dims > 1)
-//   //     fmt::print(stderr, "Supplied rank dimensions don't match
-//   total number
-//   //     of ranks!");
+  Logger::print_debug("Domain created with dimensions {} x {} x {}",
+                      dimx, dimy, dimz);
 
-//   // Start to create dims from scratch
-//   int dims[3] = {1, 1, 1};
-//   for (int i = 0; i < ndims; ++i) dims[i] = 0;
+  // Initialize domain_info
+  m_domain_info.dim = m_super_grid->dim();
+  // m_domain_info.rank_map.resize(dimz);
+  m_domain_info.rank = m_comm->world().rank();
+  m_domain_info.cart_pos.x = m_comm->cartesian().coord(0);
+  if (m_domain_info.dim >= 2)
+    m_domain_info.cart_pos.y = m_comm->cartesian().coord(1);
+  if (m_domain_info.dim >= 3)
+    m_domain_info.cart_pos.z = m_comm->cartesian().coord(2);
 
-//   // m_comm->cartesian().create_dims(num_nodes, ndims, dims);
+  // Process neighbor rank information
+  for (int i = 0; i < 3; i++) {
+    m_domain_info.cart_dims[i] = dims[i];
+    if (i < m_domain_info.dim) {
+      m_domain_info.cart_neighbor_left[i] =
+          m_comm->cartesian().neighbor_left(i);
+      Logger::print_debug_all(
+          "On rank {}, left neighbor in dir {} is({}) ",
+          m_domain_info.rank, i, m_domain_info.cart_neighbor_left[i]);
+      if (m_domain_info.cart_neighbor_left[i] == NEIGHBOR_NULL)
+        m_domain_info.is_boundary[i * 2] = true;
 
-//   if (ndims == 1) setup_domain(dims[0]);
-//   if (ndims == 2) setup_domain(dims[0], dims[1]);
-//   if (ndims == 3) setup_domain(dims[0], dims[1], dims[2]);
-// }
+      m_domain_info.cart_neighbor_right[i] =
+          m_comm->cartesian().neighbor_right(i);
+      if (m_domain_info.cart_neighbor_right[i] == NEIGHBOR_NULL)
+        m_domain_info.is_boundary[i * 2 + 1] = true;
+      Logger::print_debug_all(
+          "On rank {}, right neighbor in dir {} is ({})",
+          m_domain_info.rank, i, m_domain_info.cart_neighbor_right[i]);
+    } else {
+      m_domain_info.cart_neighbor_left[i] =
+          m_domain_info.cart_neighbor_right[i] = NEIGHBOR_NULL;
+      m_domain_info.is_boundary[i * 2] =
+          m_domain_info.is_boundary[i * 2 + 1] = true;
+    }
+  }
 
-// void
-// sim_environment::setup_domain(int dimx, int dimy, int dimz) {
-//   // First create Cartesian rank group
-//   bool periodic[3];
-//   for (int i = 0; i < 3; i++) {
-//     periodic[i] = m_conf_file.data().boundary_periodic[i];
-//   }
-//   int dims[3]{dimx, dimy, dimz};
-//   auto cartesian_ranks = m_comm->get_cartesian_members(dimx * dimy *
-//   dimz); m_comm->cartesian().create_cart(m_super_grid.dim(), dims,
-//   periodic,
-//                                   cartesian_ranks);
-//   // if (!m_comm->cartesian().is_null())
-//   //   m_domain_info.state = ProcessState::primary;
+  //   // Debug info for boundary
+  //   // Logger::print_debug_all("Rank {} has boundary info {} {} {}
+  //   {}", m_domain_info.rank, m_domain_info.is_boundary[0],
+  //   m_domain_info.is_boundary[1], m_domain_info.is_boundary[2],
+  //   m_domain_info.is_boundary[3]);
 
-//   Logger::print_debug("Domain created with dimensions {} x {} x {}",
-//   dimx, dimy, dimz);
+  // Cache whether this node is boundary on all 6 directions
+  // for (int i = 0; i < NUM_BOUNDARIES; ++i)
+  //   m_domain_info.is_boundary[i] = false;
+  // if (m_domain_info.cart_neighbor_left[0] == NEIGHBOR_NULL)
+  //   m_domain_info.is_boundary[0] = true;
+  // if (m_domain_info.cart_neighbor_right[0] == NEIGHBOR_NULL)
+  //   m_domain_info.is_boundary[1] = true;
+  // if (m_domain_info.cart_neighbor_left[1] == NEIGHBOR_NULL)
+  //   m_domain_info.is_boundary[2] = true;
+  // if (m_domain_info.cart_neighbor_right[1] == NEIGHBOR_NULL)
+  //   m_domain_info.is_boundary[3] = true;
 
-//   // Initialize domain_info
-//   m_domain_info.dim = m_super_grid.dim();
-//   // m_domain_info.rank_map.resize(dimz);
-//   m_domain_info.rank = m_comm->world().rank();
-//   m_domain_info.cart_pos.x = m_comm->cartesian().coord(0);
-//   // if (m_domain_info.dim >= 2)
-//     // m_domain_info.cart_pos.y = m_comm->cartesian().coord(1);
-//   // if (m_domain_info.dim >= 3)
-//   //   m_domain_info.cart_pos.z = m_comm->cartesian().coord(2);
+  //   // resize domain map
+  //   // for (auto& v : m_domain_info.rank_map) {
+  //   //   v.resize(dimy);
+  //   //   for (auto& u : v) {
+  //   //     u.resize(dimx, 0);
+  //   //   }
+  //   // }
 
-//   for (int i = 0; i < 3; i++) {
-//     m_domain_info.cart_dims[i] = dims[i];
-//   //   if (i < m_domain_info.dim) {
-//   //     m_domain_info.cart_neighbor_left[i] =
-//   //         m_comm->cartesian().neighbor_left(i);
-//   //     // Logger::print_debug_all("On rank {}, left neighbor in dir
-//   {} is ({})", m_domain_info.rank, i,
-//   m_domain_info.cart_neighbor_left[i]);
-//   //     if (m_domain_info.cart_neighbor_left[i] == NEIGHBOR_NULL)
-//   //       m_domain_info.is_boundary[i*2] = true;
+  //   // Populating the domain map
+  //   // int *domain_pos_x, *domain_pos_y, *domain_pos_z;
+  //   // domain_pos_x = new int[m_comm->cartesian().size()];
+  //   // domain_pos_y = new int[m_comm->cartesian().size()];
+  //   // domain_pos_z = new int[m_comm->cartesian().size()];
+  //   // // domain_pos_x[m_rank] = m_comm -> coord(0);
+  //   // // domain_pos_y[m_rank] = m_comm -> coord(1);
+  //   // // domain_pos_z[m_rank] = m_comm -> coord(2);
+  //   // m_comm->cartesian().all_gather(&m_domain_info.cart_pos.x, 1,
+  //   domain_pos_x,
+  //   //                                 1);
+  //   // m_comm->cartesian().all_gather(&m_domain_info.cart_pos.y, 1,
+  //   domain_pos_y,
+  //   //                                 1);
+  //   // m_comm->cartesian().all_gather(&m_domain_info.cart_pos.z, 1,
+  //   domain_pos_z,
+  //   //                                 1);
+  //   // for (int i = 0; i < m_comm->cartesian().size(); i++) {
+  //   //
+  //   m_domain_info.rank_map[domain_pos_z[i]][domain_pos_y[i]][domain_pos_x[i]]
+  //   //   =
+  //   //       i;
+  //   // }
 
-//   //     m_domain_info.cart_neighbor_right[i] =
-//   //         m_comm->cartesian().neighbor_right(i);
-//   //     if (m_domain_info.cart_neighbor_right[i] == NEIGHBOR_NULL)
-//   //       m_domain_info.is_boundary[i*2 + 1] = true;
-//   //     // Logger::print_debug_all("On rank {}, right neighbor in
-//   dir {} is ({})", m_domain_info.rank, i,
-//   m_domain_info.cart_neighbor_right[i]);
-//   //   } else {
-//   //     m_domain_info.cart_neighbor_left[i] =
-//   //         m_domain_info.cart_neighbor_right[i] = NEIGHBOR_NULL;
-//   //   }
-//   }
-
-//   // Debug info for boundary
-//   // Logger::print_debug_all("Rank {} has boundary info {} {} {} {}",
-//   m_domain_info.rank, m_domain_info.is_boundary[0],
-//   m_domain_info.is_boundary[1], m_domain_info.is_boundary[2],
-//   m_domain_info.is_boundary[3]);
-
-//   // Cache whether this node is boundary on all 6 directions
-//   // for (int i = 0; i < NUM_BOUNDARIES; ++i)
-//   m_domain_info.is_boundary[i] = false;
-//   // if (m_domain_info.cart_neighbor_left[0] == NEIGHBOR_NULL)
-//   m_domain_info.is_boundary[0] = true;
-//   // if (m_domain_info.cart_neighbor_right[0] == NEIGHBOR_NULL)
-//   m_domain_info.is_boundary[1] = true;
-//   // if (m_domain_info.cart_neighbor_left[1] == NEIGHBOR_NULL)
-//   m_domain_info.is_boundary[2] = true;
-//   // if (m_domain_info.cart_neighbor_right[1] == NEIGHBOR_NULL)
-//   m_domain_info.is_boundary[3] = true;
-
-//   // resize domain map
-//   // for (auto& v : m_domain_info.rank_map) {
-//   //   v.resize(dimy);
-//   //   for (auto& u : v) {
-//   //     u.resize(dimx, 0);
-//   //   }
-//   // }
-
-//   // Populating the domain map
-//   // int *domain_pos_x, *domain_pos_y, *domain_pos_z;
-//   // domain_pos_x = new int[m_comm->cartesian().size()];
-//   // domain_pos_y = new int[m_comm->cartesian().size()];
-//   // domain_pos_z = new int[m_comm->cartesian().size()];
-//   // // domain_pos_x[m_rank] = m_comm -> coord(0);
-//   // // domain_pos_y[m_rank] = m_comm -> coord(1);
-//   // // domain_pos_z[m_rank] = m_comm -> coord(2);
-//   // m_comm->cartesian().all_gather(&m_domain_info.cart_pos.x, 1,
-//   domain_pos_x,
-//   //                                 1);
-//   // m_comm->cartesian().all_gather(&m_domain_info.cart_pos.y, 1,
-//   domain_pos_y,
-//   //                                 1);
-//   // m_comm->cartesian().all_gather(&m_domain_info.cart_pos.z, 1,
-//   domain_pos_z,
-//   //                                 1);
-//   // for (int i = 0; i < m_comm->cartesian().size(); i++) {
-//   //
-//   m_domain_info.rank_map[domain_pos_z[i]][domain_pos_y[i]][domain_pos_x[i]]
-//   //   =
-//   //       i;
-//   // }
-
-//   // delete[] domain_pos_x;
-//   // delete[] domain_pos_y;
-//   // delete[] domain_pos_z;
-// }
+  //   // delete[] domain_pos_x;
+  //   // delete[] domain_pos_y;
+  //   // delete[] domain_pos_z;
+}
 
 // void
 // sim_environment::set_initial_condition(InitialCondition* ic) {
@@ -282,50 +261,33 @@ sim_environment::setup_local_grid(Grid& local_grid,
 //   if (m_domain_info.is_boundary[pos]) {
 //     Logger::print_debug_all("Rank {} Adding boundary condition at pos
 //     {}", m_domain_info.rank, pos); m_bc.add_fieldBC(bc);
-//   }
-// }
+//   }// }
 
 // void
 // sim_environment::add_ptcBC(ptcBC* bc) {
 //   m_bc.add_ptcBC(bc);
 // }
 
-// void
-// sim_environment::setup_local_grid(Grid &local_grid, const Grid
-// &super_grid, const DomainInfo& info) {
-//   local_grid = super_grid;
-//   auto& local_mesh = local_grid.mesh();
-//   const auto& super_mesh = super_grid.mesh();
-//   if (info.cart_dims[0] > 0) {
-//     local_mesh.dims[0] =
-//         super_mesh.reduced_dim(0) / info.cart_dims[0] + 2 *
-//         super_mesh.guard[0];
-//     local_mesh.sizes[0] /= info.cart_dims[0];
-//   }
-//   if (info.cart_dims[1] > 0) {
-//     local_mesh.dims[1] =
-//         super_mesh.reduced_dim(1) / info.cart_dims[1] + 2 *
-//         super_mesh.guard[1];
-//     local_mesh.sizes[1] /= info.cart_dims[1];
-//   }
-//   if (info.cart_dims[2] > 0) {
-//     local_mesh.dims[2] =
-//         super_mesh.reduced_dim(2) / info.cart_dims[2] + 2 *
-//         super_mesh.guard[2];
-//     local_mesh.sizes[2] /= info.cart_dims[2];
-//   }
+void
+sim_environment::setup_local_grid() {
+  for (int d = 0; d < 3; d++) {
+    if (m_domain_info.cart_dims[d] > 0) {
+      // local_mesh.dims[d] = super_mesh.reduced_dim(d) /
+      // info.cart_dims[d] +
+      //                     2 * super_mesh.guard[d];
+      // local_mesh.sizes[d] /= info.cart_dims[d];
+      // local_mesh.lower[d] += info.cart_pos[d] *
+      // local_mesh.reduced_dim(d) * local_mesh.delta[d];
 
-//   local_mesh.lower[0] += info.cart_pos.x * local_mesh.reduced_dim(0)
-//   *
-//                          local_mesh.delta[0];
-//   local_mesh.lower[1] += info.cart_pos.y * local_mesh.reduced_dim(1)
-//   *
-//                          local_mesh.delta[1];
-//   local_mesh.lower[2] += info.cart_pos.z * local_mesh.reduced_dim(2)
-//   *
-//                          local_mesh.delta[2];
-//   local_grid.gen_config();
-// }
+      // We adjust local params, and use these params to init the grid
+      m_params.N[d] /= m_domain_info.cart_dims[d];
+      m_params.size[d] /= m_domain_info.cart_dims[d];
+      m_params.lower[d] += m_domain_info.cart_pos[d] * m_params.size[d];
+    }
+  }
+  m_grid.reset(new Grid());
+  m_grid->init(m_params);
+}
 
 // void
 // sim_environment::apply_initial_condition(cu_sim_data &data) {
@@ -335,14 +297,14 @@ sim_environment::setup_local_grid(Grid& local_grid,
 //     return;
 //   }
 
-//   data.E.initialize([this, &data](int n, Scalar x1, Scalar x2, Scalar
-//   x3){
+//   data.E.initialize([this, &data](int n, Scalar x1, Scalar x2,
+//   Scalar x3){
 //       return this->initial_condition().E(n, x1, x2, x3,
 //       data.E.grid().mesh());
 //     });
 
-//   data.B.initialize([this, &data](int n, Scalar x1, Scalar x2, Scalar
-//   x3){
+//   data.B.initialize([this, &data](int n, Scalar x1, Scalar x2,
+//   Scalar x3){
 //       return this->initial_condition().B(n, x1, x2, x3,
 //       data.B.grid().mesh());
 //     });
