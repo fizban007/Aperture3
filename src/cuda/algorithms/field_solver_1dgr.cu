a0299f6306b531cea562326420e81101ab613263
fizban007 <fizban007@gmail.com>
2019-06-23 16:13:22 -0400

    Added 1dgr field solver

--- src/cuda/algorithms/field_solver_1dgr.cu
+++ src/cuda/algorithms/field_solver_1dgr.cu
@@ -1,9 +1,10 @@
 #include "cuda/constant_mem.h"
-#include "cuda/core/cu_sim_data1d.h"
-#include "cuda/core/field_solver_1dgr.h"
+#include "sim_data.h"
+#include "sim_environment.h"
+#include "algorithms/field_solver_1dgr.h"
 #include "cuda/cudaUtility.h"
-// #include "cuda/ptr_util.h"
 #include "cuda/utils/pitchptr.cuh"
+#include "utils/logger.h"
 
 namespace Aperture {
 
@@ -11,8 +12,8 @@ namespace Kernels {
 
 __global__ void
 update_e_1dgr(pitchptr<Scalar> e1, pitchptr<Scalar> e3,
-              pitchptr<Scalar> j1, pitchptr<Scalar> rho,
-              Grid_1dGR_dev::mesh_ptrs mesh_ptrs, Scalar dt) {
+              pitchptr<Scalar> j1,
+              Grid_1dGR::mesh_ptrs mesh_ptrs, Scalar dt) {
   for (size_t i = blockIdx.x * blockDim.x + threadIdx.x;
        i < dev_mesh.dims[0]; i += blockDim.x * gridDim.x) {
     // Scalar j0 = mesh_ptrs.j0[i] * mesh_ptrs.dpsidth[i];
@@ -36,21 +37,24 @@ update_e_1dgr(pitchptr<Scalar> e1, pitchptr<Scalar> e3,
 
 }  // namespace Kernels
 
-field_solver_1dgr_dev::field_solver_1dgr_dev() {}
+field_solver_1dgr::field_solver_1dgr(sim_environment& env) : m_env(env) {}
 
-field_solver_1dgr_dev::~field_solver_1dgr_dev() {}
+field_solver_1dgr::~field_solver_1dgr() {}
 
 void
-field_solver_1dgr_dev::update_fields(cu_sim_data1d& data, double dt,
+field_solver_1dgr::update_fields(sim_data& data, double dt,
                                      double time) {
   Logger::print_info("Updating fields");
 
-  const Grid_1dGR_dev &grid =
-      *dynamic_cast<const Grid_1dGR_dev *>(data.grid.get());
+  const Grid_1dGR &grid =
+      *dynamic_cast<const Grid_1dGR *>(&m_env.local_grid());
   auto mesh_ptrs = grid.get_mesh_ptrs();
-  Kernels::update_e_1dgr<<<256, 512>>>(data.E.ptr(0), data.E.ptr(2),
-                                       data.J.ptr(0), data.Rho[0].ptr(),
-                                       mesh_ptrs, dt);
+  Kernels::update_e_1dgr<<<256, 512>>>(
+      // data.E.ptr(0), data.E.ptr(2), data.J.ptr(0),
+      get_pitchptr(data.E.data(0)),
+      get_pitchptr(data.E.data(2)),
+      get_pitchptr(data.J.data(0)),
+      mesh_ptrs, dt);
   CudaCheckError();
 }
 
