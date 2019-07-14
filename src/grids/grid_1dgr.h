d38a993844680cb232d3e6effb505facbe9d9e7c
fizban007 <fizban007@gmail.com>
2019-07-14 12:30:30 -0400

    Added computation of alpha in output routine

--- src/grids/grid_1dgr.h
+++ src/grids/grid_1dgr.h
@@ -22,7 +22,6 @@ class Grid_1dGR : public Grid {
 
   mesh_ptrs get_mesh_ptrs() const;
 
- private:
   array<Scalar> m_D1, m_D2, m_D3, m_alpha, m_theta;
   array<Scalar> m_K1, m_K1_j, m_j0, m_agrr, m_agrf, m_rho0;
   array<Scalar> m_gamma_rr, m_gamma_ff, m_beta_phi, m_B3B1;
