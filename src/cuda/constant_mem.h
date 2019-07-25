306c2d87a104563398f64acc04b65c9d145ca86e
fizban007 <fizban007@gmail.com>
2019-07-25 13:52:44 -0400

    Update on pulsar code and radiative functions

--- src/cuda/constant_mem.h
+++ src/cuda/constant_mem.h
@@ -8,17 +8,17 @@
 namespace Aperture {
 
 // This is the simulation parameters in constant memory
-extern __device__ __constant__ SimParamsBase dev_params;
+extern __constant__ SimParamsBase dev_params;
 // This is the mesh structure in constant memory
-extern __device__ __constant__ Quadmesh dev_mesh;
+extern __constant__ Quadmesh dev_mesh;
 // This is the charge for each species, up to 8 because we use 3 bits to
 // represent species
-extern __device__ __constant__ float dev_charges[8];
+extern __constant__ float dev_charges[8];
 // This is the mass for each species, up to 8 because we use 3 bits to
 // represent species
-extern __device__ __constant__ float dev_masses[8];
+extern __constant__ float dev_masses[8];
 // This is the structure for background fields
-extern __device__ __constant__ FieldData dev_bg_fields;
+extern __constant__ FieldData dev_bg_fields;
 // This is the MPI rank of the current GPU
 extern __device__ uint64_t dev_rank;
 // This is a counter for tracked particles
