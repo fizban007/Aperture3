34e49b5e10e0951bafa7ea621bd7763d2b2f9d6b
fizban007 <fizban007@gmail.com>
2020-03-09 11:17:30 -0400

    Updated cuda-aware mpi support

--- src/cuda/sim_environment.cpp
+++ src/cuda/sim_environment.cpp
@@ -2,6 +2,8 @@
 #include "sim_environment_impl.hpp"
 #include <cuda_runtime.h>
 
+#include "mpi-ext.h" /* Needed for CUDA-aware check */
+
 namespace Aperture {
 
 void
@@ -21,22 +23,39 @@ sim_environment::send_array_guard_cells_single_dir(
   Index send_idx(0, 0, 0);
   send_idx[dim] = (dir == -1 ? mesh.guard[dim]
                              : mesh.dims[dim] - 2 * mesh.guard[dim]);
+
   m_send_buffers[dim].copy_from(array, send_idx, Index(0, 0, 0),
                                 m_send_buffers[dim].extent(),
-                                (int)cudaMemcpyDeviceToHost);
+#if defined(MPIX_CUDA_AWARE_SUPPORT) && MPIX_CUDA_AWARE_SUPPORT
+                                (int)cudaMemcpyDeviceToDevice
+#else
+                                (int)cudaMemcpyDeviceToHost
+#endif
+                                );
 
-  MPI_Sendrecv(m_send_buffers[dim].host_ptr(),
-               m_send_buffers[dim].size(), m_scalar_type, dest, 0,
-               m_recv_buffers[dim].host_ptr(),
-               m_recv_buffers[dim].size(), m_scalar_type, origin, 0,
-               m_cart, &status);
+#if defined(MPIX_CUDA_AWARE_SUPPORT) && MPIX_CUDA_AWARE_SUPPORT
+  auto send_ptr = m_send_buffers[dim].dev_ptr();
+  auto recv_ptr = m_recv_buffers[dim].dev_ptr();
+#else
+  auto send_ptr = m_send_buffers[dim].host_ptr();
+  auto recv_ptr = m_recv_buffers[dim].host_ptr();
+#endif
+
+  MPI_Sendrecv(send_ptr, m_send_buffers[dim].size(), m_scalar_type,
+               dest, 0, recv_ptr, m_recv_buffers[dim].size(),
+               m_scalar_type, origin, 0, m_cart, &status);
 
   if (status.MPI_SOURCE != MPI_PROC_NULL) {
     Index recv_idx(0, 0, 0);
     recv_idx[dim] = (dir == -1 ? mesh.dims[dim] - mesh.guard[dim] : 0);
     array.copy_from(m_recv_buffers[dim], Index(0, 0, 0), recv_idx,
                     m_recv_buffers[dim].extent(),
-                    (int)cudaMemcpyHostToDevice);
+#if defined(MPIX_CUDA_AWARE_SUPPORT) && MPIX_CUDA_AWARE_SUPPORT
+                    (int)cudaMemcpyDeviceToDevice
+#else
+                    (int)cudaMemcpyHostToDevice
+#endif
+                    );
   }
 }
 
@@ -58,11 +77,24 @@ sim_environment::send_add_array_guard_cells_single_dir(
   send_idx[dim] = (dir == -1 ? 0 : mesh.dims[dim] - mesh.guard[dim]);
   m_send_buffers[dim].copy_from(array, send_idx, Index(0, 0, 0),
                                 m_send_buffers[dim].extent(),
-                                (int)cudaMemcpyDeviceToHost);
+#if defined(MPIX_CUDA_AWARE_SUPPORT) && MPIX_CUDA_AWARE_SUPPORT
+                                (int)cudaMemcpyDeviceToDevice
+#else
+                                (int)cudaMemcpyDeviceToHost
+#endif
+                                );
 
-  MPI_Sendrecv(m_send_buffers[dim].host_ptr(),
+#if defined(MPIX_CUDA_AWARE_SUPPORT) && MPIX_CUDA_AWARE_SUPPORT
+  auto send_ptr = m_send_buffers[dim].dev_ptr();
+  auto recv_ptr = m_recv_buffers[dim].dev_ptr();
+#else
+  auto send_ptr = m_send_buffers[dim].host_ptr();
+  auto recv_ptr = m_recv_buffers[dim].host_ptr();
+#endif
+
+  MPI_Sendrecv(send_ptr,
                m_send_buffers[dim].size(), m_scalar_type, dest, 0,
-               m_recv_buffers[dim].host_ptr(),
+               recv_ptr,
                m_recv_buffers[dim].size(), m_scalar_type, origin, 0,
                m_cart, &status);
 
@@ -98,14 +130,10 @@ sim_environment::setup_env_extra() {
   for (int i = 0; i < num_ptc_buffers; i++) {
     visit_struct::for_each(
         m_ptc_buf_ptrs[i], m_ptc_buffers[i].data(),
-        [](const char *name, auto &v1, const auto &v2) {
-          v1 = v2;
-        });
+        [](const char *name, auto &v1, const auto &v2) { v1 = v2; });
     visit_struct::for_each(
         m_ph_buf_ptrs[i], m_ph_buffers[i].data(),
-        [](const char *name, auto &v1, const auto &v2) {
-          v1 = v2;
-        });
+        [](const char *name, auto &v1, const auto &v2) { v1 = v2; });
   }
 }
 
@@ -126,7 +154,6 @@ sim_environment::setup_device() {
   }
   m_dev_id = m_domain_info.rank % n_devices;
   cudaSetDevice(m_dev_id);
-
 }
 
 }  // namespace Aperture
