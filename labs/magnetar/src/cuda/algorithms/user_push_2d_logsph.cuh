23e8b5cada59cc066115ac865bce5f2226066731
fizban007 <fizban007@gmail.com>
2020-03-19 11:32:56 -0400

    moved radiation calculation into user radiative function

--- labs/magnetar/src/cuda/algorithms/user_push_2d_logsph.cuh
+++ labs/magnetar/src/cuda/algorithms/user_push_2d_logsph.cuh
@@ -107,75 +107,84 @@ user_push_2d_logsph(data_ptrs& data, size_t idx, Scalar dt,
     if (Eph > dev_params.E_ph_min && gamma > gamma_thr_B) {
       // flag = flag |= bit_or(ParticleFlag::emit_photon);
       ptc.flag[idx] = (flag | bit_or(ParticleFlag::emit_photon));
-    } else if (dev_params.rad_cooling_on) {
-      // Process resonant drag
-      Scalar p_mag_signed = sgn(pdotB) * sgn(B1) * std::abs(pdotB) / B;
-      // printf("p_mag_signed is %f\n", p_mag_signed);
-      Scalar g = sqrt(1.0f + p_mag_signed * p_mag_signed);
-      Scalar mu = std::abs(B1 / B);
-      Scalar y = (B / dev_params.BQ) /
-                 (dev_params.star_kT * (g - p_mag_signed * mu));
-      // printf("g is %f, y is %f\n", g, y);
-      if (y < 20.0f && y > 0.0f) {
-        // printf("y is %f\n", y);
-        Scalar coef = dev_params.res_drag_coef * y * y * y /
-                      (r * r * (std::exp(y) - 1.0f));
-        // printf("coef is %f\n", coef);
-        // printf("drag coef is %f\n", dev_params.res_drag_coef);
-        Scalar D = coef * (g * mu - p_mag_signed);
-        if (B1 < 0.0f) D *= -1.0f;
-        // printf("D is %f\n", D);
-        p1 += dt * B1 * D / B;
-        p2 += dt * B2 * D / B;
-        p3 += dt * B3 * D / B;
-        // printf("drag on p1 is %f\n", dt * B1 * D / B);
-        gamma = sqrt(1.0f + p1 * p1 + p2 * p2 + p3 * p3);
-
-        Scalar Ndot = std::abs(coef * (1.0f - p_mag_signed * mu / g));
-        // printf("gamma after cooling is %f\n", gamma);
-        // printf("p is (%f, %f, %f)\n", p1, p2, p3);
-        // printf("pitch angle is %f\n", pitch_angle);
-        Scalar angle =
-            acos(sgn(pdotB) * (B1 * cos(theta) - B2 * sin(theta)) / B);
-        // Scalar theta_p =
-        //     2.0f * CONST_PI * curand_uniform(&localState);
-        Scalar theta_p = CONST_PI * curand_uniform(&state);
-        Scalar phi_p = 2.0f * CONST_PI * curand_uniform(&state);
-        Scalar u = std::cos(theta_p);
-        Scalar beta = sqrt(1.0f - 1.0f / square(g));
-        // angle = angle + sgn(theta_p - CONST_PI) *
-        //                     std::acos((u + beta) / (1.0f + beta *
-        //                     u));
-        // angle = angle + (2.0f*phi_p - 1.0f) * std::acos((u + beta)
-        // / (1.0f + beta * u));
-        Scalar cos_angle =
-            std::cos(angle) * std::cos(theta_p) +
-            std::sin(angle) * std::sin(theta_p) * std::cos(phi_p);
-        angle = std::acos(cos_angle);
-        auto& ph_flux = data.ph_flux;
-        Scalar Eph =
-            (g - std::abs(p_mag_signed) * u) *
-            (1.0f - 1.0f / sqrt(1.0f + 2.0f * B / dev_params.BQ));
-        // if (p1 > 0.0f && (Eph < 2.0f || B < 0.1 * dev_params.BQ)) {
-        if (p1 > 0.0f && gamma > 1.5f && Eph < 2.0f) {
-          Eph = std::log(std::abs(Eph)) / std::log(10.0f);
-          if (Eph > 2.0f) Eph = 2.0f;
-          if (Eph < -6.0f) Eph = -6.0f;
-          int n0 = ((Eph + 6.0f) / 8.02f * (ph_flux.p.xsize - 1));
-          if (n0 < 0) n0 = 0;
-          if (n0 >= ph_flux.p.xsize) n0 = ph_flux.p.xsize - 1;
-          int n1 = (std::abs(angle) / (CONST_PI + 1.0e-5)) *
-                   (ph_flux.p.ysize - 1);
-          if (n1 < 0) n1 = 0;
-          if (n1 >= ph_flux.p.ysize) n1 = ph_flux.p.ysize - 1;
-          auto w = ptc.weight[idx];
-          atomicAdd(&ph_flux(n0, n1), Ndot * dt * w);
-          // printf("n0 is %d, n1 is %d, Ndot is %f, ph_flux is %f\n",
-          // n0,
-          //        n1, Ndot, ph_flux(n0, n1));
-        }
-      }
     }
+    // } else if (dev_params.rad_cooling_on) {
+    //   // Process resonant drag
+    //   Scalar p_mag_signed = sgn(pdotB) * sgn(B1) * std::abs(pdotB) / B;
+    //   // printf("p_mag_signed is %f\n", p_mag_signed);
+    //   Scalar g = sqrt(1.0f + p_mag_signed * p_mag_signed);
+    //   Scalar mu = std::abs(B1 / B);
+    //   Scalar y = (B / dev_params.BQ) /
+    //              (dev_params.star_kT * (g - p_mag_signed * mu));
+    //   // printf("g is %f, y is %f\n", g, y);
+    //   if (y < 20.0f && y > 0.0f) {
+    //     // printf("y is %f\n", y);
+    //     Scalar coef = dev_params.res_drag_coef * y * y * y /
+    //                   (r * r * (std::exp(y) - 1.0f));
+    //     // printf("coef is %f\n", coef);
+    //     // printf("drag coef is %f\n", dev_params.res_drag_coef);
+    //     Scalar D = coef * (g * mu - p_mag_signed);
+    //     if (B1 < 0.0f) D *= -1.0f;
+    //     // printf("D is %f\n", D);
+    //     p1 += dt * B1 * D / B;
+    //     p2 += dt * B2 * D / B;
+    //     p3 += dt * B3 * D / B;
+    //     if (p1 != p1 || p2 != p2 || p3 != p3) {
+    //         printf(
+    //             "NaN detected in resonant cooling! p1 is %f, p2 is %f, p3 is %f, gamma "
+    //             "is %f\n",
+    //             p1, p2, p3, gamma);
+    //         asm("trap;");
+    //         // p1 = p2 = p3 = 0.0f;
+    //     }
+    //     // printf("drag on p1 is %f\n", dt * B1 * D / B);
+    //     gamma = sqrt(1.0f + p1 * p1 + p2 * p2 + p3 * p3);
+
+    //     Scalar Ndot = std::abs(coef * (1.0f - p_mag_signed * mu / g));
+    //     // printf("gamma after cooling is %f\n", gamma);
+    //     // printf("p is (%f, %f, %f)\n", p1, p2, p3);
+    //     // printf("pitch angle is %f\n", pitch_angle);
+    //     Scalar angle =
+    //         acos(sgn(pdotB) * (B1 * cos(theta) - B2 * sin(theta)) / B);
+    //     // Scalar theta_p =
+    //     //     2.0f * CONST_PI * curand_uniform(&localState);
+    //     Scalar theta_p = CONST_PI * curand_uniform(&state);
+    //     Scalar phi_p = 2.0f * CONST_PI * curand_uniform(&state);
+    //     Scalar u = std::cos(theta_p);
+    //     // Scalar beta = sqrt(1.0f - 1.0f / square(g));
+    //     // angle = angle + sgn(theta_p - CONST_PI) *
+    //     //                     std::acos((u + beta) / (1.0f + beta *
+    //     //                     u));
+    //     // angle = angle + (2.0f*phi_p - 1.0f) * std::acos((u + beta)
+    //     // / (1.0f + beta * u));
+    //     Scalar cos_angle =
+    //         std::cos(angle) * std::cos(theta_p) +
+    //         std::sin(angle) * std::sin(theta_p) * std::cos(phi_p);
+    //     angle = std::acos(cos_angle);
+    //     auto& ph_flux = data.ph_flux;
+    //     Scalar Eph =
+    //         (g - std::abs(p_mag_signed) * u) *
+    //         (1.0f - 1.0f / sqrt(1.0f + 2.0f * B / dev_params.BQ));
+    //     // if (p1 > 0.0f && (Eph < 2.0f || B < 0.1 * dev_params.BQ)) {
+    //     if (p1 > 0.0f && gamma > 1.5f && Eph < 2.0f) {
+    //       Eph = std::log(std::abs(Eph)) / std::log(10.0f);
+    //       if (Eph > 2.0f) Eph = 2.0f;
+    //       if (Eph < -6.0f) Eph = -6.0f;
+    //       int n0 = ((Eph + 6.0f) / 8.02f * (ph_flux.p.xsize - 1));
+    //       if (n0 < 0) n0 = 0;
+    //       if (n0 >= ph_flux.p.xsize) n0 = ph_flux.p.xsize - 1;
+    //       int n1 = (std::abs(angle) / (CONST_PI + 1.0e-5)) *
+    //                (ph_flux.p.ysize - 1);
+    //       if (n1 < 0) n1 = 0;
+    //       if (n1 >= ph_flux.p.ysize) n1 = ph_flux.p.ysize - 1;
+    //       auto w = ptc.weight[idx];
+    //       atomicAdd(&ph_flux(n0, n1), Ndot * dt * w);
+    //       // printf("n0 is %d, n1 is %d, Ndot is %f, ph_flux is %f\n",
+    //       // n0,
+    //       //        n1, Ndot, ph_flux(n0, n1));
+    //     }
+    //   }
+    // }
   }
   ptc.p1[idx] = p1;
   ptc.p2[idx] = p2;
